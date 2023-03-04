8feff0947cf8373c3e9655abebaf3e48aa2461ff
AlexSlz <30480995+AlexSlz@users.noreply.github.com>
2023-03-04 16:04:45 +0200

    first

---
+++ ClientApp/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+interface Data {
+  name: string
+}
+
+const result = ref<Data>({ name: '' })
+onMounted(async () => {
+  result.value = await (await fetch('/api')).json()
+})
+</script>
+
+<template>
+  <h1>{{ result.name }}</h1>
+</template>
